2496384e3dd8c0c8279d3a0dec337eb870c8dbea
Raúl Ahumada <raulahumada00@gmail.com>
2022-07-20 00:31:09 -0300

    new Changes

--- components/Header.js
+++ components/Header.js
@@ -1,22 +1,11 @@
-/* eslint-disable jsx-a11y/alt-text */
 import Link from 'next/link';
 import Image from 'next/image';
-import logo from '../images/logo.png';
 
 export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
-      <Link href="/">
-        <a>
-          <Image
-            src={logo}
-            width="150"
-            height="150"
-            className=" rounded-full block "
-          />
-        </a>
-      </Link>
-      <p className="text-3xl dark:text-white text-center">
+      <div className="w-12 h-12 rounded-full block mx-auto mb-4 bg-gradient-conic from-gradient-3 to-gradient-4" />
+      <p className="text-2xl dark:text-white text-center">
         <Link href="/">
           <a>{name}</a>
         </Link>
